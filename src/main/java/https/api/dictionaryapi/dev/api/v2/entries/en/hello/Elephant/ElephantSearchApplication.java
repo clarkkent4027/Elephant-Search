ef93134ae14e1ae60a65f32f0d6f0e1fe463a554
lvl0_module2 <zoey.lestyk@gmail.com>
2024-06-30 10:12:33 -0700

    initial

---
+++ src/main/java/https/api/dictionaryapi/dev/api/v2/entries/en/hello/Elephant/ElephantSearchApplication.java
@@ -0,0 +1,13 @@
+package https.api.dictionaryapi.dev.api.v2.entries.en.hello.Elephant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElephantSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElephantSearchApplication.class, args);
+	}
+
+}
