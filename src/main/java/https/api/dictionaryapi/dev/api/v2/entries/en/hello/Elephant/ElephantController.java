9f135bff422247550232adff36aa7e456029a878
league <Blake.Christian.Hay@gmail.com>
2024-07-17 12:00:11 -0700

    Milestone7.1

--- src/main/java/https/api/dictionaryapi/dev/api/v2/entries/en/hello/Elephant/ElephantController.java
+++ src/main/java/https/api/dictionaryapi/dev/api/v2/entries/en/hello/Elephant/ElephantController.java
@@ -1,5 +1,6 @@
 package https.api.dictionaryapi.dev.api.v2.entries.en.hello.Elephant;
 
+import https.api.dictionaryapi.dev.api.v2.entries.en.hello.Elephant.service.ElephantService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
--- src/main/java/https/api/dictionaryapi/dev/api/v2/entries/en/hello/Elephant/ElephantRepository.java
+++ src/main/java/https/api/dictionaryapi/dev/api/v2/entries/en/hello/Elephant/ElephantRepository.java
@@ -1,4 +1,31 @@
 package https.api.dictionaryapi.dev.api.v2.entries.en.hello.Elephant;
 
+import org.springframework.stereotype.Repository;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Repository
 public class ElephantRepository {
+    private final WebClient webClient;
+    private static final String baseURL = "https.api.dictionaryapi.dev.api.v2.entries.en.hello.Elephant.service";
+
+public ElephantRepository (){
+    webClient = WebClient
+            .builder()
+            .baseUrl(baseURL)
+                .build();
+}
+
+    public String getResults(String query) {
+        return webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .queryParam("fo", "json")
+                        .queryParam("at", "results")
+                        .queryParam("q", query)
+                        .build()
+                )
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+    }
+
 }
