655b4598a0dba35d0e516804e23d793ab1bb0652
league <clarkkent4027@gmail.com>
2024-08-25 11:33:28 -0700

    Milestone13.1

--- src/intTest/java/https.api.dictionaryapi.dev.api.v2.entries.en.hello.Elephant/presentation/HomeControllerIntTest.java
+++ src/intTest/java/https.api.dictionaryapi.dev.api.v2.entries.en.hello.Elephant/presentation/HomeControllerIntTest.java
@@ -1,6 +1,27 @@
 package java.https.api.dictionaryapi.dev.api.v2.entries.en.hello.Elephant.presentation;
 
-public class HomeControllerIntTest
-{
+import https.api.dictionaryapi.dev.api.v2.entries.en.hello.Elephant.presentation.HomeController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+//import org.springframework.test.web.servlet.MockMvc;
+//
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(HomeController.class)
+class HomeControllerIntTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void givenController_WhenHome_ThenReturnMovedPermanentlyAndRedirect() throws Exception {
+        mockMvc.perform(get("/"))
+                .andDo(print())
+                .andExpect(status().isMovedPermanently())
+                .andExpect(redirectedUrl("swagger-ui.html"));
+    }
 
 }
