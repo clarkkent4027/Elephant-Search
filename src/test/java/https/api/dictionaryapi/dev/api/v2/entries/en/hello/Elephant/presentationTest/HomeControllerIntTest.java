254ee8ae23b6f71c0c9813da1f9e94b803e63e47
league <clarkkent4027@gmail.com>
2024-09-01 10:34:07 -0700

    Milestone13.3

--- src/test/java/https/api/dictionaryapi/dev/api/v2/entries/en/hello/Elephant/presentationTest/HomeControllerIntTest.java
+++ src/test/java/https/api/dictionaryapi/dev/api/v2/entries/en/hello/Elephant/presentationTest/HomeControllerIntTest.java
@@ -1,15 +1,18 @@
-package java.https.api.dictionaryapi.dev.api.v2.entries.en.hello.Elephant.presentation;
+package https.api.dictionaryapi.dev.api.v2.entries.en.hello.Elephant.presentationTest;
 
 import https.api.dictionaryapi.dev.api.v2.entries.en.hello.Elephant.presentation.HomeController;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-//import org.springframework.test.web.servlet.MockMvc;
-//
+import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(HomeController.class)
 class HomeControllerIntTest {
